62400c09e40b095942a9b1d52d4427066a727038
RoshanJose <roshanstark007@gmail.com>
2020-05-26 11:31:18 +0530

    Fixed extra spaces and fill color bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let logol = document.querySelectorAll('#logo path');
+let logol = document.querySelectorAll('#logo-1 path');
 
 for (let i = 0; i < logol.length; i++) {
   console.log(`Letter ${i} is ${logol[i].getTotalLength()}`);
